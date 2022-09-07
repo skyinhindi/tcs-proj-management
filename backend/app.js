fce1ee6f661b69a0c015a278cdcce23f86c1f143
Aakash Chaudhary <aakash.199303185@muj.manipal.edu>
2022-09-07 16:42:19 +0530

    Added .env config

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
@@ -11,14 +12,11 @@ server.use(express.static("public"));
 server.use(express.urlencoded({ extended: true }));
 server.use(cors({ origin: "*" }));
 
-const dbURI =
-  process.env.dbURI ||
-  "mongodb+srv://admin:adminPass1234@cluster0.ybwqgl5.mongodb.net/?retryWrites=true&w=majority";
-
+const dbURI = process.env.dbURI;
 const db = mongoose
   .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then((result) => {
-    server.listen(process.env.PORT || 5000);
+    server.listen(process.env.PORT);
     console.log("connected to database...");
   })
   .catch((err) => console.log(err));
